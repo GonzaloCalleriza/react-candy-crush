84b99da65700945b5105f964903a28f1173c99cc
gonzalogonzalo <calleriza.gonzalo@gmail.com>
2022-03-05 12:31:22 +0100

    Finished refilling process, working on movement

--- src/App.js
+++ src/App.js
@@ -39,10 +39,10 @@ const App = () => {
   }
 
   const checkForRowOfThree = () => {
-    for(let i=0; i<=64; i++){
+    for(let i=0; i<64; i++){
       const rowOfThree = [i, i+1, i+2]; 
       const decidedColor = currentColorArrangement[i];
-      const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64];
+      const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 62, 63];
 
       if(notValid.includes(i)) continue
 
@@ -53,16 +53,34 @@ const App = () => {
   }
 
   const checkForRowOfFour = () => {
-    for(let i=0; i<=64; i++){
+    for(let i=0; i<64; i++){
       const rowOfFour = [i, i+1, i+2, i+3]; 
       const decidedColor = currentColorArrangement[i];
-      const notValid = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55, 62, 63, 64];
+      const notValid = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55, 61, 62, 63];
 
       if(notValid.includes(i)) continue
 
       if(rowOfFour.every(square => currentColorArrangement[square] === decidedColor)) {
         rowOfFour.forEach(square => currentColorArrangement[square] = '');
       };
+    }
+  }
+
+  const moveIntoSquareBelow = () => {
+    for(let i = 0; i < 64 - width; i++){
+
+      const firstRow = [0, 1, 2, 3, 4, 5, 6, 7]
+      const isFirstRow = firstRow.includes(i)
+
+      if(isFirstRow && currentColorArrangement[i] === ''){
+        let randomNumber = Math.floor(Math.random() * candyColors.length());
+        currentColorArrangement[i] = candyColors[randomNumber];
+      }
+
+      if((currentColorArrangement[i + width]) === ''){
+        currentColorArrangement[i + width] = currentColorArrangement[i];
+        currentColorArrangement[i] = '';
+      }
     }
   }
 
@@ -85,10 +103,11 @@ const App = () => {
       checkForColumnOfThree();
       checkForRowOfFour();
       checkForRowOfThree();
+      moveIntoSquareBelow(); 
       setCurrentColorArrangement([...currentColorArrangement])
     }, 100)
     return () => clearInterval(timer)
-  }, [checkForColumnOfFour, checkForRowOfFour, checkForColumnOfThree, checkForRowOfThree, currentColorArrangement])
+  }, [checkForColumnOfFour, checkForRowOfFour, checkForColumnOfThree, checkForRowOfThree, moveIntoSquareBelow, currentColorArrangement])
 
   return (
     <div className="app">
